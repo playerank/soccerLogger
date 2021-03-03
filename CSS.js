28f4b43062a5e70ff051182d6fbca05b16796f26
mrg1913 <41339122+mrg1913@users.noreply.github.com>
2021-03-03 17:57:14 +0100

    Add files via upload

---
+++ CSS.js
@@ -0,0 +1,11 @@
+function set_color_row(obj, color){
+
+    document.getElementById("name_event").style.backgroundColor = color
+    document.getElementById("time_event_start").style.backgroundColor = color
+    document.getElementById("time_event_end").style.backgroundColor = color
+    document.getElementById("player_number").style.backgroundColor = color
+    document.getElementById("player_name").style.backgroundColor = color
+    document.getElementById("active_team").style.backgroundColor = color
+    document.getElementById("extra_tag").style.backgroundColor = color
+
+}
